3619e3f6836d3d7084132c775b39ab9601579c1a
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-05-10 00:08:53 +0300

    fix: error https

--- src/js/contants.js
+++ src/js/contants.js
@@ -1,5 +1,5 @@
 const KEY_OMDB = '6f237dd4';
-const URL_OMDB = 'http://www.omdbapi.com/';
+const URL_OMDB = 'https://www.omdbapi.com/';
 const KEY_Y = 'trnsl.1.1.20200422T111801Z.e0d39b73a715a384.a2a2b921649e23f209f29f7200aebd93340f4270';
 const URL_Y = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
 const IMGS = {
