4ccf20d3b57b5fc9402caaa9ac1491e91c02ea1c
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-05-09 16:06:13 +0300

    feat: change constants

--- src/js/contants.js
+++ src/js/contants.js
@@ -2,4 +2,15 @@ const KEY_OMDB = '6f237dd4';
 const URL_OMDB = 'http://www.omdbapi.com/';
 const KEY_Y = 'trnsl.1.1.20200422T111801Z.e0d39b73a715a384.a2a2b921649e23f209f29f7200aebd93340f4270';
 const URL_Y = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
-export { KEY_OMDB, URL_OMDB, KEY_Y, URL_Y };
+const IMGS = {
+  star: './src/assets/img/star.svg',
+  patch: './src/assets/img/oscar.jpg',
+};
+
+export {
+  KEY_OMDB,
+  URL_OMDB,
+  KEY_Y,
+  URL_Y,
+  IMGS,
+};
